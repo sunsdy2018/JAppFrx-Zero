b2f2fd349d8ff7e4138679d71523620e5abe01e7
sunsdy2018 <38144147@qq.com>
2021-07-07 17:54:10 +0800

    created

---
+++ src/main/java/com/sunsdy2018/jappfxzero/JappfxzeroApplication.java
@@ -0,0 +1,13 @@
+package com.sunsdy2018.jappfxzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JappfxzeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JappfxzeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunsdy2018/jappfxzero/JappfxzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunsdy2018.jappfxzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JappfxzeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
